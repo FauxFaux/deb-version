94a235257e30998383e65c41275a4476925d2dfd
Chris West (Faux) <git@goeswhere.com>
2017-09-12 22:15:59 +0100

    test sorting all versions in Debian today

--- rust/tests/against-libapt.rs
+++ rust/tests/against-libapt.rs
@@ -4,6 +4,7 @@ extern crate deb_version;
 extern crate lazy_static;
 #[macro_use]
 extern crate quickcheck;
+extern crate rand;
 extern crate regex;
 
 use std::cmp::Ordering;
@@ -81,4 +82,26 @@ quickcheck! {
             libapt(&left.version, &right.version) == compare_versions(&left.version, &right.version)
         )
     }
+}
+
+#[test]
+fn sort_everything() {
+    use rand::Rng;
+    use rand::thread_rng;
+
+    let everything = include_str!("../../all-debian-versions.lst");
+    let mut everything: Vec<&str> = everything.split('\n').collect();
+    thread_rng().shuffle(&mut everything);
+    assert!(everything.len() > 30_000);
+
+    // shuffle then sort_by(_stable) as 1.00.00 == 1.0.0, which breaks this test
+    // but hopefully not the real world
+
+    let mut apt = everything.clone();
+    apt.sort_by(|left, right| libapt(left, right));
+
+    let mut us: Vec<&str> = everything.clone();
+    us.sort_by(|left, right| compare_versions(left, right));
+
+    assert_eq!(apt, us);
 }
